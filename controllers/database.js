df073d7ed1058a8100a30884702f500109cdbeae
olga <goncherenkoola@gmail.com>
2022-07-10 18:19:40 +0300

    first commit

--- controllers/database.js
+++ controllers/database.js
@@ -1,21 +1,20 @@
-const mysql = require('mysql2');
-const config = require('../config/dev');
+const mysql = require("mysql2");
+const config = require("../config/dev");
 
 const pool = mysql.createPool({
-    host: config.DB_HOST,
-    user: config.DB_USER,
-    password: config.DB_PASSWORD,
-    database: config.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 5,
-    queueLimit: 0
+  host: config.DB_HOST,
+  user: config.DB_USER,
+  database: config.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 5,
+  queueLimit: 0,
 });
 
 async function query(sql, values) {
-    const promisePool = pool.promise();
-    return [rows, fields] = await promisePool.query(sql, values);
+  const promisePool = pool.promise();
+  return ([rows, fields] = await promisePool.query(sql, values));
 }
 
 module.exports = {
-    query,
-}
+  query,
+};
