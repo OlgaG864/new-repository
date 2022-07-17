46a88a1190ed0cfd5f882c11d3ca2181c2dc5375
olga <goncherenkoola@gmail.com>
2022-07-17 20:33:44 +0300

    adding exports product

--- client-angular/src/app/products/products.component.ts
+++ client-angular/src/app/products/products.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { Product } from '../shared/types';
+import { FilePath, Product } from '../shared/types';
 import { ApiService } from '../core/api.service';
+import { environment } from 'src/environments/environment';
 
 @Component({
     selector: 'app-products',
@@ -26,5 +27,14 @@ export class ProductsComponent implements OnInit {
     }
     imagePath(img: string | null): string {
         return !img ? '' : `../../assets/images/${img}`;
+    }
+
+    exportProductsData() {
+        this.apiService.exportProducts().subscribe({
+            next: (data: FilePath) => {
+                window.open(`${environment.serverUrl}/${data.name}`);
+            },
+            error: (err) => console.error(err),
+        });
     }
 }
