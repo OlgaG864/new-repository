7e7cb5f4f67c736b8cd3712d0783ec33804f826f
olga <goncherenkoola@gmail.com>
2022-07-27 17:39:36 +0300

    login routes

--- routes/orders.js
+++ routes/orders.js
@@ -1,14 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const om = require('../controllers/orders');
-const fileMgmt = require('../shared/fileMgmt');
+const om = require("../controllers/orders");
+const fileMgmt = require("../shared/fileMgmt");
 
-router.get('/home', function (req, res, next) {
-    const filePath = fileMgmt.getHtmlFilePath('orders-home.html');
-    res.sendFile(filePath);
+router.get("/home", function (req, res, next) {
+  const filePath = fileMgmt.getHtmlFilePath("orders-home.html");
+  res.sendFile(filePath);
 });
 
-router.get('/', om.ordersList);
-router.get('/export', om.exportOrders);
+router.get("/", om.ordersList);
+router.get("/export", om.exportOrders);
 
 module.exports = router;
